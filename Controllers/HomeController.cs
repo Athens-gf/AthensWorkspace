abd11d780c5a9eeac1e6a683ee534ff228c4242c
Athens_gf <shamira0322@gmail.com>
2026-03-24 21:45:14 +0900

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics;
+using AthensWorkspace.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AthensWorkspace.Controllers;
+
+public class HomeController : Controller
+{
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+    public IActionResult Index() => View();
+
+    public IActionResult Privacy() => View();
+}
